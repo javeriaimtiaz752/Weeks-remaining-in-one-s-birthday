bf269489789895a12ef04ac1db6c316fc42640d7
Javeria Imtiaz <javeriaimtiaz752@gmail.com>
2024-07-12 16:11:06 +0500

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,3 @@
+days_until_birthday = int(input("How many days until your birthday?"))
+weeks_until_birthday = round(days_until_birthday / 7)
+print("Approximately", weeks_until_birthday, "weeks until your birthday.")
